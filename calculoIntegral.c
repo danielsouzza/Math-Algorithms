ca1a52bc37ae61eca50bab3993e1057249734269
Kamille Santos <kamillecaetano93@gmail.com>
2022-10-22 17:37:22 -0300

    ADD - Função logbase

--- calculoIntegral.c
+++ calculoIntegral.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef double tipo_func(double x );
+typedef double tipo_func(double x);
+
+// Para Logaritmos de outras bases
+double logbase(double a, double base)
+{
+    return log(a) / log(base);
+}
 
 // Log²(x)
-double log1(double x){
-    return pow(log(x),2);
+double log1(double x)
+{
+    return pow(log(x), 2);
 }
 
 // Log(ax+b)
-double log2(double x){
-    return log(10*x + 5);
+double log2(double x)
+{
+    return log(10 * x + 5);
 }
 
 // Somatório de integração
-double somatorio(double n, double a,double b, double (*func)(double)){
+double somatorio(double n, double a, double b, double (*func)(double))
+{
 
     double area = 0;
     double delta = (b - a) / n; // delta x
 
-    while(n-- > 0){
-        area += func((2*a+delta)/2); // Calcula o ponto médio e passa para a função
-        a+= delta; // passa para a próxima extremidade do retângulo 
+    while (n-- > 0)
+    {
+        area += func((2 * a + delta) / 2); // Calcula o ponto médio e passa para a função
+        a += delta;                        // passa para a próxima extremidade do retângulo
     }
     return area * delta; // Calcula base x altura e retorna o resultado
 }
 
+void main()
+{
 
-void main(){
-
-    double n,a,b;
-    tipo_func * vetor_func[4]; // Array de função
+    double n, a, b;
+    tipo_func *vetor_func[4]; // Array de função
     vetor_func[0] = log1;
     vetor_func[1] = log2;
 
-
     printf("Digite o número de retângulos e o intervalo[a,b]: ");
     scanf("%lf %lf %lf", &n, &a, &b);
-    printf("A área aproximadamente para log1: %f\n", somatorio(n,a,b,vetor_func[0]));
-    printf("A área aproximadamente para log2: %f\n", somatorio(n,a,b,vetor_func[1]));
+    printf("A área aproximadamente para log1: %f\n", somatorio(n, a, b, vetor_func[0]));
+    printf("A área aproximadamente para log2: %f\n", somatorio(n, a, b, vetor_func[1]));
 }
