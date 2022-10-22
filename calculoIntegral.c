02ccc28be3bbaad7400b5e724491e6ec4d1b9377
danielsouzza <daniel.lima.souzza@gmail.com>
2022-10-21 22:15:35 -0300

    ADD - Log²(x) && log(ax+b)

--- calculoIntegral.c
+++ calculoIntegral.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-
-// Função x² -> só de teste ainda
 typedef double tipo_func(double x );
 
+// Log²(x)
 double log1(double x){
-    return sqrt(log10(x));
+    return pow(log(x),2);
+}
+
+// Log(ax+b)
+double log2(double x){
+    return log(10*x + 5);
 }
 
 // Somatório de integração
 double somatorio(double n, double a,double b, double (*func)(double)){
+
     double area = 0;
     double delta = (b - a) / n; // delta x
 
-    for(int i = 0; i < n; i++){
+    while(n-- > 0){
         area += func((2*a+delta)/2); // Calcula o ponto médio e passa para a função
         a+= delta; // passa para a próxima extremidade do retângulo 
     }
@@ -23,8 +28,9 @@ double somatorio(double n, double a,double b, double (*func)(double)){
 
 
 void main(){
+
     double n,a,b;
-    tipo_func * vetor_func[4];
+    tipo_func * vetor_func[4]; // Array de função
     vetor_func[0] = log1;
     vetor_func[1] = log2;
 
